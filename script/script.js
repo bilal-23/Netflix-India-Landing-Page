025c5b706cff63b8e1452ada54b729dc8bf3bc39
Bilal <mansuribilal101@gmail.com>
2021-03-07 20:26:14 +0530

    + - sign FAQ Dropdown

--- script/script.js
+++ script/script.js
@@ -6,6 +6,11 @@ function showDropdown() {
     const x = this.classList.value;
     const y = dropdowns[x];
     y.classList.toggle('show');
+    if (y.classList.contains('show')) {
+        this.textContent = '-';
+    } else {
+        this.textContent = '+';
+    }
 }
 
 plus.forEach(plus => plus.addEventListener('click', showDropdown));
